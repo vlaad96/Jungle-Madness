f925ec4f91f53ff52df545ec32e55b3be9d0c87d
Vladislav Bulantsev <vladislavbv96@gmail.com>
2019-11-18 10:16:03 +0100

    Revert "Entities + Pathfinding modules created (anything done)"
    This reverts commit 68a0abf59688b6fac4721261f2531db23a516be3.

--- Source/j1App.h
+++ Source/j1App.h
@@ -14,9 +14,7 @@ class j1Audio;
 class j1Scene;
 class j1Map;
 class j1Collision;
-class j1Player;//HASTOBEDELETED
-class j1PathFinding;
-class j1EntityManager;
+class j1Player;
 
 class j1App
 {
@@ -88,9 +86,7 @@ public:
 	j1Scene*			scene;
 	j1Map*				map;
 	j1Collision*		col;
-	j1Player*			player;//HASTOBEDELETED
-	j1PathFinding*		pathfinding;
-	j1EntityManager*    entities;
+	j1Player*			player;
 
 private:
 
